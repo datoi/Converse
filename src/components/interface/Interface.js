7e2322908cc8156db41d14576706831e31bcf2ef
datoi <dato.tadiashvili13@gmail.com>
2024-05-27 22:03:18 +0400

    first commit

---
+++ src/components/interface/Interface.js
@@ -0,0 +1,21 @@
+const Interface = () => {
+    return (
+        <div>
+            <header>
+                <div className='d-flex'>
+                    <div>
+                        <div><i className="fa-solid fa-bars"></i></div>
+                        <h1>Support inbox</h1>
+                    </div>
+                    <div>
+                        <h1>My inbox</h1>
+                        <h1></h1>
+                    </div>
+                </div>
+            </header>
+        </div>
+
+    )
+}
+
+export default Interface
